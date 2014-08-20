a12f59f044de93ff745cf68f4aecbb95cc1e3e4c
teddymcw <teddymcw@gmail.com>
2014-08-19 18:49:50 -0700

    modified lesson plan and readme for first iteration of lesson

--- func-lesson.md
+++ func-lesson.md
@@ -1,55 +1,94 @@
+Objectives:
+-Formally introduce functional programming paradigm in python, compare to imperative, and make it easy
+-Execute some "valuable" text processing
+-Have some fun with words
 
-1. If you’ve done excel then you have done functional programming
--finding ‘perfect primes’ in excel, numbers, libreoffice
 
-2. Where and how functional programming is incorporated into python, a mostly imperative language
-https://docs.python.org/2/howto/functional.html
--forces you to break down problem into small chunks
--easier to debug
-etc.
+1. If you’ve done excel then you have done functional programming. Formal definitions (20 min)
+-basic functions in excel, numbers, libreoffice
 
+-plug in a couple of sets (or lists) into excel and apply a few functions
+-test against below definitions
+
+Referential Transparency: What can you see in the data/objects being processed?
 
 The principle of referential transparency (RT) is this:
 An expression e is referentially transparent if for all programs p every occurrence of e in p can be replaced with the result of evaluating e, without affecting the observable result of p
 
 We'll use this statement to test whether our following code is functional or imperative.
 
-Note about "true" functional languages and their use of strong typing and recursion.
+Plug in excel example.
 
-quickly compare to imperative programming paradigm (r object in requests library example) and how this relates to OOP.
-what does a purely functional language look like?
+2. Where and how functional programming is incorporated into python, a mostly imperative language (10 min)
 
---retrieve dataset 
-build upon previous lesson using requests library and request data from reddit API
---quickly parse dataset into basic list
+-discuss pros and cons, why and when?
 
+-official resource: https://docs.python.org/2/howto/functional.html
 
-3. iterators and generators: using examples of cleaning and manipulating raw text data into consummable information for end user.
+Why even use a functional style of programming when imperative is just fine!?
+-forces you to break down problem into small chunks
+-easier to debug
+etc.
 
-a yield statement can halt at an iteration whereas a return statement 
+-can you apply these rules to English grammar?
+
+3. iterators and generators: using basic examples (20 min)
+
+intro to list comps, iterator terminology and generators using examples
+
+iterables v. iterators:
+eg: can be iterated v. the iterator itself.
+keep in mind that the iterator variable is also different than the value the iterator is pointing to
 
 Two common operations on an iterator’s output are 
     1) performing some operation for every element
-    2) selecting a subset of elements that meet some condition. 
+    2) selecting a subset of elements that meet some condition
 
-subroutines are another word for function.
-Subroutines are entered at one point and exited at another point (the top of the function, and a return statement), but coroutines can be entered, exited, and resumed at many different points (the yield statements). 
-
-#neato tidbit
-The dict() constructor can accept an iterator that returns a finite stream of (key, value) tuples: 
->>> L = [('Italy', 'Rome'), ('France', 'Paris'), ('US', 'Washington DC')]
->>> dict(iter(L)){'Italy': 'Rome', 'US': 'Washington DC', 'France': 'Paris'} 
 
 list comps and gen expressions borrowed directly from Haskell
 
-On executing the yield expression, the generator outputs the value of i, similar to a return statement. The big difference between yield and a return statement is that on reaching a yield the generator’s state of execution is suspended and local variables are preserved. On the next call to the generator’s .next() method, the function will resume executing. 
+list comprehensions:
+list comps are "syntactic sugar" in that they don't add any functionality but they make expressions easier to write and in this case to read and comprehend 
 
-If a generator function calls return or reaches the end its definition, a StopIteration exception is raised.
-Once a generator has been exhausted, calling next() on it will result in an error, so you can only consume all the values of a generator once.
+generators:   definite added functionality
+a yield statement can halt at an iteration whereas a return statement returns an entire object
+a generator is an iterable that is a stream in memory, it can only be iterated through once
+
+subroutines: another word for function. Subroutines are entered at one point and exited at another point (the top of the function, and a return statement)
+
+coroutines: a type of subroutine BUT can be entered, exited, and resumed at many different points (the yield statements). Generators can be referred to as coroutines in the python domain.
+
+note that it is rare to find the case where using a generator is not better than using a list. In fact in python 3 many functions that did produce lists were replaced by their generator equivalent such as range and xrange or filter and ifilter. 
+
+
+4. basic appiclation: compare cleaning text methods on larger text corpuses converted into iterables for processing (10 min)
+
+-introduce text to work with
+-discuss methodology and necesary steps for valuable analysis
+
+5. deeper dive application: cleaning and manipulating raw text data into consummable information for end user (40 min)
+
+-thinking about what we want to extract
+-how should we design are functions
+-write
+-apply / play!
+
+6. fun with itertools (10 min)
+
+-applying a few functions that would produce some type of analysis or humor
+-tour the itertools library and discuss purpose
+
+7. further: (infinity)
+
+-lambda functions
+
+-include control statements to take care of the StopIteration exception when a generator has exhausted its possible iterators.
  
-include control 
 try:
 
 except:
 
-finally: 
+finally:
+
+-what other metrics or analysis can we provide combining what we have written thus far and what we are aware of just in the stdlib available to us?
+-how would we spur cretivity when feeling stuck? 
--- func_lesson.py
+++ func_lesson.py
@@ -1,3 +1,83 @@
+#direct from iPython Notebook
+
+#1 If you’ve done excel then you have done functional programming
+#  Port excel work into python script
+
+line1 = range(1, 9)
+line2 = range(10, 50, 5)
+#print test to see what we have correlates to excel data
+print(line1, line2)
+
+total = sum(line1) + sum(line2)
+
+#have states changed?
+print(line1, line2)
+
+#or
+print(sum(range(1,9)) + sum(range(10,50,5)))
+
+#have states changed?
+print(line1, line2)
+
+#or 
+from itertools import chain
+print(sum(chain(range(1,9), range(10,50,5))))
+
+#have states changed?
+print(line1, line2)
+ 
+#2 discussion of pros and cons of the above code and keep these principles in mind for the folowing
+
+
+#3 brief overview of generators and comprehensions
+#  intro to iterators terminology, list comps, and generators
+iterable = range(20)
+#i is the iterator
+for i in iterable:
+    print(i*2)
+
+alpha_iter = "abcdefghijksmellonop"
+for i in alpha_iter:
+    print(i*2)
+
+#but if we want it in a data structure that we can manipulate further
+def get_double_letters(ltrs):
+    doubles = list()
+    for i in alpha_iter:
+        doubles.append(i*2)
+    return doubles
+
+#list comprehensions
+double_letters = [ltr*2 for ltr in alpha_iter]
+
+#generator function
+def gen_double_letters(ltrs):
+    for i in alpha_iter:
+        yield i*2    
+ 
+#won't work btw
+#for i in alpha_iter:
+#    yield i*2        
+
+print(get_double_letters(alpha_iter))
+print(gen_double_letters(alpha_iter))
+
+gen_object = gen_double_letters(alpha_iter)
+
+gen_object.next()
+gen_object.next()
+
+list(gen_object)
+
+#gen expressions! write them backwards at first
+def gen_dubs(ltrs):
+    for i in (ltr*2 for ltr in ltrs):
+        yield i
+
+#4 basic appiclation: compare cleaning text methods on larger text corpuses converted into iterables for processing
+#let's apply this to our sample text 
+
+#just test that we are in proper directory with proper files
 with open("obama_09.txt") as fp:
     o_09 = fp.read()
 with open("obama_14.txt") as fp:
@@ -14,7 +94,7 @@ def imp_clean_text(text, remove_words=(stop_words, support_words)):
     full_text_ls = full_text.split()
     clean_text = list()
     for word in full_text_ls:
-        if word in stop_words or support_words:
+        if word not in stop_words and support_words:
             clean_text.append(word) 
     return clean_text
 
@@ -22,11 +102,8 @@ def imp_clean_text(text, remove_words=(stop_words, support_words)):
 def func_clean_text(text, remove_words=(stop_words, support_words)):
     with open(text) as fp:
         full = fp.read()
-    for i in (word for word in full.split() if word in stop_words or support_words):
+    for i in (word for word in full.split() if word not in stop_words and support_words):
         yield i
-
-#brief overview of generators and comprehensions
-
 
 imp_14 = imp_clean_text("obama_14.txt", remove_words=(stop_words, support_words))
 func_14 = func_clean_text("obama_14.txt", remove_words=(stop_words, support_words))
@@ -40,11 +117,22 @@ print(imp_14 == list(func_14))
 print(imp_14 == list(func_clean_text("obama_14.txt", remove_words=(stop_words, support_words))))
 
 
+# %timeit magic magic method is only for ipython users
+#%timeit imp_clean_text("obama_09.txt")
+#%timeit func_clean_text("obama_09.txt")
 
+#use time.timeit method in python interpreter
+#from time import timeit
+#imp09_timed = timeit("imp_09", setup=None, number=100000)
+#imp14_timed = timeit("imp_14", setup=None, number=100000)
 
+        
+        
 
+#5 deeper dive application: cleaning and manipulating raw text data into consummable information for end user
+#quick itertools intro and begin text processing
 
-#heavier duty gen expressions, probably omit
+#everything in itertools yields a generator!
 
 from itertools import chain
 def func_clean_text(text, remove_words=(stop_words, support_words)):
@@ -54,22 +142,24 @@ def func_clean_text(text, remove_words=(stop_words, support_words)):
         yield i
 
 
-
+#heavier duty gen expressions
 
 def gen_tokens(text):
     """ str -> gen 
-    call nltk's work_tokenize function on our text,
-    which differs from just splitting our list by spaces
+    call split on our text when its a string
     """
-    for word in text.split():
-        yield word
+    with open(text) as fp:
+        fp = fp.read()
+        for word in fp.split():
+            yield word
 
 def gen_cleaner_words(text):
     """ str -> str 
     removes commonly 'attached' punctuation marks such as ',''.''*word*' etc.
+    note that this function does not remove any words from iterable
     """
-    for i in list(gen_tokens(text)):
-        yield i.translate(None, "!@#$%^&*().,[]+=-_`~<>?")
+    for i in gen_tokens(text):
+        yield i.translate(None, "!@#$%^&*().,[]+=-_`~<>?:;")
 
 def gen_lowercase_tokens(text):
     """ str -> gen 
@@ -86,6 +176,13 @@ def gen_ns_words(text):
     for i in (word for word in gen_cleaner_words(text) if word.lower() not in stop_words):
         yield i
 
+def gen_nsup_words_only(text):
+    """str -> gen
+    generate words not in support words, mostly a utility function
+    """
+    for i in (word for word in gen_tokens(text) if word.lower() not in support_words):
+        yield i
+        
 #note for this module's purpose the default type for stop_words will be lowercase
 def gen_ns_and_nsup_words(text):
     """ str -> gen 
@@ -93,6 +190,30 @@ def gen_ns_and_nsup_words(text):
     """
     for i in (word for word in gen_ns_words(text) if word.lower() not in support_words):
         yield i
+
+#Analysis functions
+def get_text_len_and_content_len(text):
+    """ str -> float, float
+    return length of text list with stop_words omitted and length of 'content'
+    defined as non 'support words' """ 
+    num_ns_tokens = float(len(list(gen_ns_words(text))))
+    num_content_tokens = float(len(list(gen_ns_and_nsup_words(text))))
+    return (num_ns_tokens, num_content_tokens)
+
+def get_content_percentage(text):
+    """ str -> float
+    number of words in text - stop words and support words / number of tokens in text. 
+    Note the difference between this ratio and the lexical diversity ratio, 
+    which only accounts for unique words.
+    """  
+    try: 
+        result = float(len(list(gen_ns_and_nsup_words(text)))) / float(len(list(gen_tokens(text))))
+        result = round(result, 2)
+    except ZeroDivisionError:
+        pass
+        result = 0
+    finally:
+        return result
 
 #print(list(chain(stop_words, support_words)))
 
@@ -104,3 +225,70 @@ f2 = gen_ns_and_nsup_words("obama_14.txt")
 #print(list(f2))
 
 #print "up" in f1
+
+get_content_percentage("obama_09.txt")
+get_text_len_and_content_len("obama_09.txt")
+print len(list(gen_cleaner_words("obama_09.txt")))
+
+def gen_nsup_words_only(text):
+    """str -> gen
+    generate words not in support words, mostly a utility function
+    """
+    for i in (word for word in gen_tokens(text) if word.lower() not in support_words):
+        yield i
+
+gen_nsup_words_only("obama_09.txt")
+
+len(list(gen_ns_words("obama_09.txt")))
+len(list(gen_ns_and_nsup_words("obama_09.txt"))) 
+
+get_text_len_and_content_len("obama_09.txt")
+
+#quick detour into the collections module
+from collections import Counter
+ctr_obama_09 = Counter(gen_ns_and_nsup_words("obama_09.txt"))
+ctr_obama_14 = Counter(gen_ns_and_nsup_words("obama_14.txt"))
+
+#print(ctr_obama_09)
+#for ipython users (otherwise use inspect module)
+#ctr_obama_09?
+
+#now for a bit more comparative analysis with a basic data type
+set_obama_09 = set(gen_ns_and_nsup_words("obama_09.txt"))
+set_obama_14 = set(gen_ns_and_nsup_words("obama_14.txt"))
+
+#what can we do?
+print len(list(gen_tokens("obama_09.txt")))
+print len(set_obama_09)
+
+print len(list(gen_tokens("obama_14.txt")))
+print len(set_obama_14)
+
+#NLP metric a la George Lakoff
+def get_rough_keyword_ratio(text):
+    return float(len(set(gen_ns_and_nsup_words(text)))) / float(len(list(gen_tokens(text)))) * 100
+
+get_rough_keyword_ratio("obama_09.txt")
+get_rough_keyword_ratio("obama_14.txt")
+
+
+#6 fun with itertools
+
+#a little fun with itertools
+from itertools import *
+#ifilter(len() > 10, gen_ns_and_nsup_words("obama_09.txt")) 
+#is this helpful or gratuitous 
+
+#ctr_obama_09[:10]  Type Error
+
+for i in islice(ctr_obama_09, 10):
+    print(i)
+
+#frozenset(ctr_obama_09)
+
+#funny pairs a la Louis Van Ahn's captcha?
+#print(list(izip(set_obama_09, set_obama_14)))
+
+#well its hard to apply these principles to text, most are meant for numbers
+#see http://pymotw.com/2/itertools/
+
