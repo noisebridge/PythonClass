f90fac30454537ec0727371ffc54bde4a1e2f78d
Trent Robbins <robbintt@gmail.com>
2014-08-26 19:54:56 -0700

    Put the code in __main__ for lesson 5 guess-a-number example.

--- 5_control_statements_and_exceptions_hierarchy/guess-a-number-ex.py
+++ 5_control_statements_and_exceptions_hierarchy/guess-a-number-ex.py
@@ -3,23 +3,25 @@ This is an example of the control structures.
 """
 
 
-result = ""
-our_number = 21
+if __name__ == "__main__":
+
+    result = ""
+    our_number = 21
 
 
-def test_number(answer):
+    def test_number(answer):
 
-    answer = int(answer)
+        answer = int(answer)
 
-    if answer == our_number:
-        return "got it right"
-    elif answer > our_number:
-        return "nope, lower"
-    else:
-        return "nope, higher"
+        if answer == our_number:
+            return "got it right"
+        elif answer > our_number:
+            return "nope, lower"
+        else:
+            return "nope, higher"
 
 
-while result != "got it right":
-    result = test_number(raw_input("Choose a number:"))
-    print result
+    while result != "got it right":
+        result = test_number(raw_input("Choose a number:"))
+        print result
 
