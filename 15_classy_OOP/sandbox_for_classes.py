98e3658e18b64aac0125df1a9fe8cb73dae78aa9
teddymcw <teddymcw@gmail.com>
2014-10-07 14:41:41 -0700

    few examples added

--- 15_classy_OOP/sandbox_for_classes.py
+++ 15_classy_OOP/sandbox_for_classes.py
@@ -1,1 +1,29 @@
 #stub
+
+class Pillow(object):
+    """This is a comfy pillow"""
+
+    def __init__(self, upholstery='linen', stuffing_type='cotton', stuffing_capacity=100, size=None):
+        self.upholstery = upholstery
+        self.stuffing_type = stuffing_type
+        self.stuffing_capacity = stuffing_capacity
+        self.size = (9,24)
+        self.active = True
+
+    def get_area(self):
+        return self.size[0] * self.size[1]
+
+    def hit_someone(self, hit_object=10):
+        """self.int -> self.int 
+        lose stuffing capacity points equal to size of hit object"""
+
+        self.stuffing_capacity -= hit_object
+
+        if self.stuffing_capacity <= 0:
+            self.active = True
+
+
+    def __str__(self):
+        #note that reversing two methods would recurse
+        return "this is an override: {}".format(self.__repr__())
+
