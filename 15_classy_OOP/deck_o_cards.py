179d56c814ae3d1b2ef8f0d647a08fdc19fb02c9
teddymcw <teddymcw@gmail.com>
2014-10-08 00:44:55 -0700

    finally added deck_o_cards

--- 15_classy_OOP/deck_o_cards.py
+++ 15_classy_OOP/deck_o_cards.py
@@ -1,4 +1,6 @@
-class Card:
+#from Think Python 
+
+class Card(object):
     suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
     ranks = ["narf", "Ace", "2", "3", "4", "5", "6", "7",
              "8", "9", "10", "Jack", "Queen", "King"]
@@ -10,9 +12,81 @@ class Card:
     def __str__(self):
         return (self.ranks[self.rank] + " of " + self.suits[self.suit])
 
-threeOfClubs = Card(0, 3)
-jackOfDiamonds = Card(1, 11)
-jackOfDiamonds.suits[1] = "Swirly Whales"
-print(jackOfDiamonds)
+    def cmp(self, other):
+        # Check the suits
+        if self.suit > other.suit:
+            return 1
+        if self.suit < other.suit:
+            return -1
+        # Suits are the same... check ranks
+        if self.rank > other.rank:
+            return 1
+        if self.rank < other.rank:
+            return -1
+        # Ranks are the same... it's a tie
+        return 0
 
-#from Think Python 
+    def __eq__(self, other):
+        return self.cmp(other) == 0
+
+    def __le__(self, other):
+        return self.cmp(other) <= 0
+
+    def __ge__(self, other):
+        return self.cmp(other) >= 0
+
+    def __gt__(self, other):
+        return self.cmp(other) > 0
+
+    def __lt__(self, other):
+        return self.cmp(other) < 0
+
+    def __ne__(self, other):
+        return self.cmp(other) != 0
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                self.cards.append(Card(suit, rank))
+
+    def __str__(self):
+        s = ""
+        for card in self.cards:
+            s += str(card) + '\n'
+        return s
+
+    def shuffle(self):
+        import random
+        random.shuffle(self.cards)
+
+    def remove(self, card):
+        if card in self.cards:
+            self.cards.remove(card)
+            return True
+        else:
+            return False
+
+    def pop(self):
+        return self.cards.pop()
+
+    def is_empty(self):
+        return self.cards == []
+
+    #reimplementation of shuffle function
+    """def shuffle(self):
+        import random
+        numCards = len(self.cards)
+        for i in range(numCards):
+            j = random.randrange(i, numCards)  # pick a card to swap with i
+            (self.cards[i], self.cards[j]) = (self.cards[j], self.cards[i])"""
+
+
+
+d = Deck()
+#can we do the below? why not?
+print(d)
+d.shuffle()
+print(d)
