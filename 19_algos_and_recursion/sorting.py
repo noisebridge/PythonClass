d929fa2e63e789d6d03fd3b9cdf39dd0b76689fb
teddymcw <teddymcw@gmail.com>
2014-10-28 12:20:32 -0700

    added merge_sort code

--- 19_algos_and_recursion/sorting.py
+++ 19_algos_and_recursion/sorting.py
@@ -1,4 +1,4 @@
-def binarySearch(alist, item):
+def binary_search(alist, item):
         if len(alist) == 0:
             return False
         else:
@@ -7,12 +7,50 @@ def binarySearch(alist, item):
               return True
             else:
               if item<alist[midpoint]:
-                return binarySearch(alist[:midpoint],item)
+                return binary_search(alist[:midpoint],item)
               else:
-                return binarySearch(alist[midpoint+1:],item)
+                return binary_search(alist[midpoint+1:],item)
     
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
 print("binary search for 1")
-print(binarySearch(testlist, 3))
+print(binary_search(testlist, 3))
 print("binary search for 2")
-print(binarySearch(testlist, 2))
+print(binary_search(testlist, 2))
+
+
+def merge_sort(alist):
+    print("Splitting ", alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i<len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j<len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+alist = [54,26,93,17,77,31,44,55,20]
+merge_sort(alist)
+print(alist)
