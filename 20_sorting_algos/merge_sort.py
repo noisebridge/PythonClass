17193e2df04761c2721693972bd146eb67ccf862
teddymcw <teddymcw@gmail.com>
2014-11-12 20:31:41 -0800

    in class algos

--- 20_sorting_algos/merge_sort.py
+++ 20_sorting_algos/merge_sort.py
@@ -6,46 +6,66 @@ This is our base case.
 
 ###
 print "merge sort"
+count = 0
+
 def merge_sort(alist):
     """list -> list 
     sorted from least to greatest
     """
 
-    print("Splitting ", alist)
-    if len(alist)>1:
-        mid = len(alist)//2
+    #print("Splitting ", alist)
+    global count 
+
+    #splitting list section 
+    if len(alist) > 1:
+        mid = len(alist) // 2
         lefthalf = alist[:mid]
         righthalf = alist[mid:]
 
         merge_sort(lefthalf)
         merge_sort(righthalf)
 
-        i=0
-        j=0
-        k=0
+        #merging splices section
+        i=0 #should be called first_value
+        j=0 #should be called second_value
+        k=0 #rebuilding list
+
+
         while i<len(lefthalf) and j<len(righthalf):
+            count += 1
             if lefthalf[i]<righthalf[j]:
+                
                 alist[k]=lefthalf[i]
                 i=i+1
             else:
                 alist[k]=righthalf[j]
                 j=j+1
+            print "alist[k]", alist[k]
             k=k+1
 
         while i<len(lefthalf):
+            count += 1
             alist[k]=lefthalf[i]
             i=i+1
             k=k+1
 
         while j<len(righthalf):
+            count += 1
             alist[k]=righthalf[j]
             j=j+1
             k=k+1
         #helper print statement
-        print("Merging ", alist)
+        #print("Merging ", alist)
 
-    return alist
+    return alist, count
 
-alist = [54,26,93,17,77,31,44,55,20]
-merge_sort(alist)
-print(alist)
+
+
+alist = [18, 18, 12, 12, 3, 1, 2, 10, 13, 17]
+ls2 = range(20,10,-1)
+#merge_sort(alist)
+
+merge_sort(ls2)
+
+#print(alist)
+print("iterations {}".format(count))
