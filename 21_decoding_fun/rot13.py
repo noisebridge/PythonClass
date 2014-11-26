a4ca10783f36a2e023e524bd2a36b19142c6f37a
Trent Robbins <robbintt@gmail.com>
2014-11-26 02:07:00 -0800

    Completed word_confidence.py implementation.

--- 21_decoding_fun/rot13.py
+++ 21_decoding_fun/rot13.py
@@ -69,8 +69,7 @@ if __name__ == '__main__':
 
     rotation_dict = assign_and_return_positions(alphabet, rotation)
 
-    #    input_string = "12345 - Alphabet Soup; Acceptable Inputs: a-z, comma, period, colon, space, hyphen, underscore, and semicolon. Uppercase letters will be converted to lowercase."
-    input_string = 'abcdez'
+    input_string = "12345 - Alphabet Soup; Acceptable Inputs: a-z, comma, period, colon, space, hyphen, underscore, and semicolon. Uppercase letters will be converted to lowercase."
     cleaned_input = clean_string(input_string.lower())
     
     print rotation_dict
--- 21_decoding_fun/word_confidence.py
+++ 21_decoding_fun/word_confidence.py
@@ -2,6 +2,14 @@
 import re
 import pickle
 
+# Two variables can be used to fine-tune the output.
+# A better solution is do weigh shorter words more lightly and longer more heavily.
+confidence_requirement = 1
+word_length_requirement = 5
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 pickle_filename = 'ngsl.pickle'
 mode = 'r'
 
@@ -37,20 +45,53 @@ The reduced form of the list will be a dictionary with 'nm' key and number of oc
 Since we know the reduced form why can't se skip the nonreduced form.
 """
 
+vetted_nonmatching_pairs = dict()
+
 # iterate over all source words for each part word
 for part_word in partially_decoded_wordlist:
+    part_word = part_word.lower()
     for source_word in ngsl_words:
+        source_word = source_word.lower()
         # only take action if the lengths match, this is an easy optimization place for later
-        if len(part_word) == len(source_word):
-            confidence = 0
-            nonmatching_pairs = []
+        # Also use a min length stopgap to avoid high frequency short words from impacting our stats.
+        if len(part_word) >= word_length_requirement and len(part_word) == len(source_word):
+            confidence = len(part_word)
+            nonmatching_pairs = dict()
             # step throuch each position in both and check matching
             for i in range(len(source_word)):
                 if part_word[i] == source_word[i]:
                     # either increase the confidence of the match
-                    confidence += 1
+                    confidence -= 1
                 else:
-                    # or add a new nonmatching pair
-                    nonmatching_pairs.append(part_word[i] + source_word[i])
+                    # test that we aren't introducing numbers.
+                    if part_word[i] in alphabet and source_word[i] in alphabet:
+                        # or add a new nonmatching pair
+                        pair = part_word[i] + source_word[i]
+                        if pair in nonmatching_pairs.keys():
+                            nonmatching_pairs[pair] += 1
+                        elif pair[::-1] in nonmatching_pairs.keys():
+                            nonmatching_pairs[pair[::-1]] += 1
+                        else:
+                            nonmatching_pairs[pair] = 1
+            # Temporary: use a static confidence to vet pairs
+            if confidence <= confidence_requirement:
+                # Increment the vetted pairs accordingly.
+                for pair, count in nonmatching_pairs.iteritems():
+                    if pair in vetted_nonmatching_pairs.keys():
+                        vetted_nonmatching_pairs[pair] += count
+                    elif pair[::-1] in vetted_nonmatching_pairs.keys():
+                        vetted_nonmatching_pairs[pair[::-1]] += count
+                    else:
+                        vetted_nonmatching_pairs[pair] = count
+                        print "New pair:", pair
 
+# Final list of each potential pair. Lets manually test this.
+print vetted_nonmatching_pairs
 
+vetted_nonm_list = list()
+for ltr, frq in vetted_nonmatching_pairs.iteritems():
+    vetted_nonm_list.append((frq, ltr))
+vetted_nonm_list.sort()
+vetted_nonm_list = vetted_nonm_list[::-1]
+
+print vetted_nonm_list
