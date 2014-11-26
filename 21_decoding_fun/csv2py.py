903b18d3fa8bab5ddb3de3f58c2428a0bedf22c1
Trent Robbins <robbintt@gmail.com>
2014-11-25 20:48:02 -0800

    Added pickle of ngsl list.

--- 21_decoding_fun/csv2py.py
+++ 21_decoding_fun/csv2py.py
@@ -1,13 +1,22 @@
 
+import pickle
 import csv
 
 mode = 'r'
 filename = 'ngsl1.01.csv'
 
+wordslist = []
 with open(filename, mode) as f:
     words = csv.reader(f)
 
+    print words
     for row in words:
-        print row
+        wordslist = row
 
+print wordslist
+
+filename = 'ngsl.pickle'
+mode = 'wb'
+with open(filename, mode) as f:
+    pickle.dump(wordslist, f)
 
