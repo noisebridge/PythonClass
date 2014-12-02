bf464a62f9a5e993b0f83180fc2a834314171f8f
teddymcw <teddymcw@gmail.com>
2014-12-02 15:20:49 -0800

    some changes from bottom-up group now working top-down

--- 21_decoding_fun/bottom_up_decode.py
+++ 21_decoding_fun/bottom_up_decode.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #correct two letter words from scrambled text into unscrambled words 
 #and cache the correct conversions
 
@@ -14,12 +16,56 @@ def get_twoltr_words(scrambled_text):
         text_two_ltrs = [word for word in t.read() if len(word) == 2]
     return text_two_ltrs
 
-print get_twoltr_words("cc.txt")
+#print "two ltr words"
+#print get_twoltr_words("mmenc.txt")
 
 
 
 
+#from collections import Counter 
 def get_longest_word(scrambled_text):
     with open(scrambled_text) as t:
-        text_two_ltrs = [word for word in t.read() if len(word) == 2]
-    return text_two_ltrs
+        # > 10 is an arbitrary number for now
+        text_list = [word for word in t.read().split() if len(word) > 10]
+        ln = 1
+        longest_word = ""
+        for word in text_list:
+            if len(word) > ln:
+                ln = len(word)
+                longest_word = word
+
+    return longest_word
+
+
+print "longest word"
+print get_longest_word("mmenc.txt")
+
+
+
+import pickle
+
+def get_eng_word_dict():
+    filename = 'ngsl.pickle'
+    mode = 'r'
+
+    with open(filename, mode) as f:
+        ngsl_words = pickle.load(f)
+        return ngsl_words
+
+def match_english_words_by_length(eng_dict, word_length):
+    eng_dict_word_len_matches = [word for word in eng_dict if len(word) == word_length]
+    return eng_dict_word_len_matches
+
+word_len = len(get_longest_word("mmenc.txt"))
+eng_dict = get_eng_word_dict()
+
+print match_english_words_by_length(eng_dict, word_len)
+
+        #text_longest_word
+    #return text_two_ltrs
+
+#clean all non-matching english words eg: words with dashes, numbers, non-alpha or nonsense words
+#take care of unicode issues
+
+
+
