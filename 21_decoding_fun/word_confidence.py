46dc10f76249f578043eb2f2b8db0d5512b53d07
Trent Robbins <robbintt@gmail.com>
2014-12-05 11:49:09 -0800

    Did some optimization of word_confidence. It didn't help very much.

--- 21_decoding_fun/word_confidence.py
+++ 21_decoding_fun/word_confidence.py
@@ -1,3 +1,8 @@
+"""
+The words need to all be lowercase and only a-z.
+We don't check that here.
+
+"""
 
 import re
 import pickle
@@ -50,47 +55,48 @@ vetted_nonmatching_pairs = dict()
 
 # iterate over all source words for each part word
 for part_word in partially_decoded_wordlist:
-    part_word = part_word.lower()
     for source_word in ngsl_words:
-        source_word = source_word.lower()
-        # only take action if the lengths match, this is an easy optimization place for later
-        # Also use a min length stopgap to avoid high frequency short words from impacting our stats.
-        if len(part_word) >= word_length_requirement and len(part_word) == len(source_word):
-            confidence = len(part_word)
-            nonmatching_pairs = dict()
-            # step throuch each position in both and check matching
-            for i in range(len(source_word)):
-                if part_word[i] == source_word[i]:
-                    # either increase the confidence of the match
-                    confidence -= 1
-                else:
-                    # test that we aren't introducing numbers.
-                    if part_word[i] in alphabet and source_word[i] in alphabet:
-                        # or add a new nonmatching pair
-                        pair = part_word[i] + source_word[i]
-                        if pair in nonmatching_pairs.keys():
-                            nonmatching_pairs[pair] += 1
-                        elif pair[::-1] in nonmatching_pairs.keys():
-                            nonmatching_pairs[pair[::-1]] += 1
-                        else:
-                            nonmatching_pairs[pair] = 1
-            # Temporary: use a static confidence to vet pairs
-            if confidence <= confidence_requirement:
-                # Increment the vetted pairs accordingly.
-                for pair, count in nonmatching_pairs.iteritems():
-                    if pair in vetted_nonmatching_pairs.keys():
-                        vetted_nonmatching_pairs[pair] += count
-                    elif pair[::-1] in vetted_nonmatching_pairs.keys():
-                        vetted_nonmatching_pairs[pair[::-1]] += count
+        len_source_word = len(source_word)
+        len_part_word = len(part_word)
+
+        # use a min length stopgap to avoid high frequency short words from impacting our stats.
+        if len_part_word >= word_length_requirement:
+            # only take action if the lengths match, this is an easy optimization place for later
+            if len_part_word == len_source_word:
+                confidence = len_part_word
+                nonmatching_pairs = dict()
+                # step throuch each position in both and check matching
+                for i in range(len_source_word):
+                    if part_word[i] == source_word[i]:
+                        # either increase the confidence of the match
+                        confidence -= 1
                     else:
-                        vetted_nonmatching_pairs[pair] = count
-                        print "New pair:", pair
+                        # or add a new nonmatching pair, in any order. the dict made here needs consolidated
+                        # for example, you could have nm and mn in this dict which should be combined.
+                        pair = part_word[i] + source_word[i]
+                        nonmatching_pairs[pair] = nonmatching_pairs.get(pair, 0) + 1
+                # use a static confidence to vet pairs
+                if confidence <= confidence_requirement:
+                    # Increment the vetted pairs accordingly.
+                    for pair, count in nonmatching_pairs.iteritems():
+                        if vetted_nonmatching_pairs.get(pair, 0) == 0:
+                            print "New pair:", pair
+                        vetted_nonmatching_pairs[pair] = vetted_nonmatching_pairs.get(pair, 0) + count
+
+clean_nonmatching_pairs = dict()
+# We need to consolidate pairs in vetted_nonmatching_pairs that need combined, such as nm and mn.
+for pair, count in vetted_nonmatching_pairs.iteritems():
+    if pair[::-1] in clean_nonmatching_pairs:
+        clean_nonmatching_pairs[pair[::-1]] += count
+    else:
+        clean_nonmatching_pairs[pair] = count
+
 
 # Final list of each potential pair. Lets manually test this.
-print vetted_nonmatching_pairs
+print clean_nonmatching_pairs
 
 vetted_nonm_list = list()
-for ltr, frq in vetted_nonmatching_pairs.iteritems():
+for ltr, frq in clean_nonmatching_pairs.iteritems():
     vetted_nonm_list.append((frq, ltr))
 vetted_nonm_list.sort()
 vetted_nonm_list = vetted_nonm_list[::-1]
