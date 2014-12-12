be9dbb545ea287ac574922257100fc3f18f47695
Trent Robbins <robbintt@gmail.com>
2014-12-11 16:40:13 -0800

    Reorganized logging examples directory 23.

---
+++ 23_logging/ex1.py
@@ -0,0 +1,16 @@
+
+
+
+
+import logging
+
+logging.basicConfig(filename='ex1.log', level='DEBUG')
+
+logging.debug("This is a string that we want to log.")
+logging.info("info: this string is cool.")
+
+with open('ex1.log', 'r') as f:
+    print f.read()
+
+
+
---
+++ 23_logging/ex2.py
@@ -0,0 +1,11 @@
+
+
+import logging
+
+logging.basicConfig(level='INFO')
+
+logging.debug("This is a string that we want to log.")
+logging.info("info: this string is cool.")
+
+
+
