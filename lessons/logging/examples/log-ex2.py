9aa2347f02cde3db4aa711f7d8217e6498bb408a
Trent Robbins <robbintt@gmail.com>
2015-01-20 22:56:57 -0800

    Added twologgers advanced example and updated logging module comments.

--- lessons/logging/examples/log-ex2.py
+++ lessons/logging/examples/log-ex2.py
@@ -9,6 +9,8 @@ logging.basicConfig(filename='ex1.log', level='DEBUG')
 logging.debug("This is a string that we want to log.")
 logging.info("info: this string is cool.")
 
+
+# Now we just read the file back to ourselves to confirm it worked.
 with open('ex1.log', 'r') as f:
     print f.read()
 
