64b6185dbb6643a30238b4cddd7b465642250ec0
Trent Robbins <robbintt@gmail.com>
2015-03-09 12:47:36 -0700

    Broke args_kwargs examples out to two examples.

--- lessons/higher_order_functions/examples/args_kwargs.py
+++ lessons/higher_order_functions/examples/args_kwargs.py
@@ -1,24 +1,14 @@
 """
 A quick example of *args and *kwargs
 """
-
-
 # Remember: explicit is better than implicit
 import this
+
 
 def dyn_function(*args, **kwargs):
     """ Accept args, kwargs which correspond to a list and a dict respectively.
     """
     
-    print("{}, {}").format(type(args), type(kwargs))
-    print("{}, {}").format(args, kwargs)
-
-
-def dyn_function_with_explicits(argname, name="myname", *args, **kwargs):
-
-    print type(argname), argname
-    print type(name), name
-
     print("{}, {}").format(type(args), type(kwargs))
     print("{}, {}").format(args, kwargs)
 
@@ -34,14 +24,6 @@ if __name__ == "__main__":
                     tomatoes=10, 
                     onions=1, 
                     garlic=3)
-
-    mylist = [1,2,3,4,5]
-    mydict = {'cilantro':2,'tomatoes':10,'onions':1,'garlic':3}
-
-    dyn_function_with_explicits(    'argument', 
-                                    dir(),
-                                    mylist,
-                                    mydict)
 
 
 
---
+++ lessons/higher_order_functions/examples/args_kwargs2.py
@@ -0,0 +1,31 @@
+"""
+A quick example of *args and *kwargs
+"""
+# Remember: explicit is better than implicit
+import this
+
+
+def dyn_function_with_explicits(argname, name="myname", *args, **kwargs):
+
+    print type(argname), argname
+    print type(name), name
+
+    print("{}, {}").format(type(args), type(kwargs))
+    print("{}, {}").format(args, kwargs)
+
+
+if __name__ == "__main__":
+
+    # Create some space
+    print "\n\n"
+
+    mylist = [1,2,3,4,5]
+    mydict = {'cilantro':2,'tomatoes':10,'onions':1,'garlic':3}
+
+    dyn_function_with_explicits(    'argument', 
+                                    dir(),
+                                    mylist,
+                                    mydict)
+
+
+
