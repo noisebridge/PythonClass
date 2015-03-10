e640d97c1f10e4c9e19a781c8bc5c361d03c2414
Trent Robbins <robbintt@gmail.com>
2015-03-09 20:06:06 -0700

    Finished decorator samples.

--- lessons/higher_order_functions/examples/decorator3.py
+++ lessons/higher_order_functions/examples/decorator3.py
@@ -1,15 +1,31 @@
 """
-So here's the special syntax for a decorator.
+So why would we use a decorator?
 
+This example is fairly contrived, but it puts some control flow
+into a decorator to catch extra input that the sample() function
+isn't written to accept.
 """
 
 
 
 def mydecorator(func):
-    print "Decorator is go!"
     """ A decorator that simply passes through a function.
     """
-    return func
+    print "Decorator is go!"
+    def function_wrapper(*args, **kwargs):
+        """
+        We'll need to run the function in the wrapper in order
+        to extend the functionality and/or modify the output.
+        """
+        # Lets intercept the return if there are too many values.
+        # I have been explicit in this compound if statement, but 
+        # remember an empty dict/list is false, so there's a better way.
+        if len(args) > 0 or len(kwargs.keys()) > 0:
+            return func()
+        else:
+            return func(*args, **kwargs)
+
+    return function_wrapper
 
 
 # Using the decorator.
@@ -18,4 +34,5 @@ def sample():
     print "instantiating sample function"
     return 'sample'
 
-print sample()
+# This would break if it wasn't wrapped/decorated!
+print sample('too', 'many', 'arguments')
