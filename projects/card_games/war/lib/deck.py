610d0d2c639f2749772601120251ba071cb85e71
Trent Robbins <robbintt@gmail.com>
2015-03-17 21:44:00 -0700

    Added Deck class and used inheritance.

--- projects/card_games/war/lib/deck.py
+++ projects/card_games/war/lib/deck.py
@@ -5,7 +5,41 @@ from collections import deque
 import random
 
 
-class StandardDeck(object):
+class Deck(object):
+    """ Contains common deck methods.
+    """
+    def __init__(self):
+        self.deck = deque()
+
+    def shuffle(self):
+        """ Randomizes the order of the deck
+        """
+        random.shuffle(self.deck)
+
+    def cut(self, cut_size):
+        """ Cuts the deck
+        """
+        self.deck.rotate(cut_size)
+
+    def deal_n_cards(self, num_to_deal=1):
+        """ Deals an iterable containing one card from the top of the deck.
+
+        This function uses pop() to deal from the right. This must be consistent through the class.
+        """
+        cards_to_deal = collections.deque()
+        for i in range(num_to_deal):
+            cards_to_deal.append(self.deck.pop())
+        return cards_to_deal
+
+    def accept_n_cards(self, cards):
+        """ Accepts an iterable of cards and puts them back in the deck. 
+
+        This function uses extendleft to accept. This must be consistent through the class.
+        """
+        self.deck.extendleft(cards)
+
+
+class StandardDeck(Deck):
     """ A standard 52-card deck. Cards are stored, ordered, in a collections.deque object.
     """
 
@@ -26,40 +60,13 @@ class StandardDeck(object):
     def __init__(self):
         """ 
         """
-        self.deck = deque()
+        # idiom from: http://stackoverflow.com/questions/576169/understanding-python-super-with-init-methods
+        super(self.__class__, self).__init__()
+
         for suit in self.suits:
             for rank in self.ranks:
                 self.deck.append(self.Card(rank, suit))
 
-    def shuffle(self):
-        """ Randomizes the order of the deck
-        """
-        random.shuffle(self.deck)
-
-    def cut(self, cut_size):
-        """ Cuts the deck
-        """
-        self.deck.rotate(cut_size)
-
-    def deal_n_cards(self, num_to_deal=1):
-        """ Deals an iterable containing one card from the top of the deck.
-
-        This function uses pop() to deal from the right. This must be consistent through the class.
-        """
-        cards_to_deal = collections.deque()
-
-        for i in range(num_to_deal):
-            cards_to_deal.append(self.deck.pop())
-
-        return cards_to_deal
-
-
-    def accept_n_cards(self, cards):
-        """ Accepts an iterable of cards and puts them back in the deck. 
-
-        This function uses extendleft to accept. This must be consistent through the class.
-        """
-        self.deck.extendleft(cards)
 
 
 
