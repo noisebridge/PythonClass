d22c0e1a76711319221de87c8e79a697bf5768e2
Trent Robbins <robbintt@gmail.com>
2015-03-31 21:14:06 -0700

    Added cut check.

--- projects/card_games/war/test.py
+++ projects/card_games/war/test.py
@@ -4,6 +4,7 @@ Unit tests for deck.py
 """
 import unittest
 import collections
+import copy
 
 import lib.deck # Module to test
 
@@ -15,20 +16,21 @@ class TestDeck(unittest.TestCase):
         """
         self.deck = lib.deck.Deck()
         self.stddeck = lib.deck.StandardDeck()
+        self.cardcount = len(self.stddeck.suits) * len(self.stddeck.ranks)
 
 
     def test_deck_is_a_deque(self):
         self.assertEqual(self.deck.deck, collections.deque())
 
-    def test_stddeck_has_52_cards(self):
-        self.assertEqual(52, len(self.stddeck.deck))
+    def test_stddeck_has_all_the_cards(self):
+        self.assertEqual(self.cardcount, len(self.stddeck.deck))
 
-    def test_each_stddeck_card_is_valid(self):
+    def test_each_stddeck_cards_are_valid(self):
         for card in self.stddeck.deck:
             self.assertIn(card.rank, self.stddeck.ranks)
             self.assertIn(card.suit, self.stddeck.suits)
 
-    def test_each_stddeck_card_is_unique(self):
+    def test_each_stddeck_cards_are_unique(self):
         for card in self.stddeck.deck:
             card_instances = 0
             for card2 in self.stddeck.deck:
@@ -36,8 +38,33 @@ class TestDeck(unittest.TestCase):
                     card_instances += 1
             self.assertEqual(1,card_instances)
 
+    def test_stddeck_cut_method(self):
+
+        self.stddeck2 = copy.deepcopy(self.stddeck)
+        # Check to make sure these are different objects (different memory)
+        self.assertNotEqual(self.stddeck2, self.stddeck)
+        
+        def check_deck_equality(deck1, deck2):
+            """ Check to make sure that the two decks have the same order of cards.
+            """
+
+            for i in xrange(len(deck1.deck)):
+                self.assertEqual(deck1.deck[i].rank, deck2.deck[i].rank)
+                self.assertEqual(deck1.deck[i].suit, deck2.deck[i].suit)
+
+        check_deck_equality(self.stddeck, self.stddeck2)
+
+        # cut both decks using rotate OR the cut method
+        for cutsize in xrange(self.cardcount):
+            self.stddeck.cut(cutsize)
+            self.stddeck2.deck.rotate(cutsize)
+            check_deck_equality(self.stddeck, self.stddeck2)
+
+        
+
+
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestDeck)
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 
