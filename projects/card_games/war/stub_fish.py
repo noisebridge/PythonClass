361e9524177e66cc8e1bc20d66224a5437095ab9
teddymcw <teddymcw@gmail.com>
2015-04-13 18:50:32 -0700

    added changes from last class

--- projects/card_games/war/stub_fish.py
+++ projects/card_games/war/stub_fish.py
@@ -19,7 +19,9 @@ class Table(object):
         #self.fishing_pot = StandardDeck()
         self.cur_player_deq = deque()
 
-
+#decision of what DS to use with hands, discuss what operations we will need 
+#and what will be most manageable getters and setters, 
+#time and memory shouldn't be of concern
 class GoFishGame(object):
     """
     """
@@ -36,23 +38,24 @@ class GoFishGame(object):
 
 
         self.table = Table()
-        self.cur_players = self.table.cur_player_deq.extend(self.player1, self.player2)
+        self.cur_players = self.table.cur_player_deq.extend([self.player1, self.player2])
         self.losers_list = list()
         
         self.fishing_pot.shuffle()
         self.deal_cards()
 
-        self.play_gofish()
+        #self.play_gofish()
 
     def deal_cards(self):
         """
         """
-
-        amt_dealt_init_hand1 = self.fishing_pot.deal_n_cards(7)
-        amt_dealt_init_hand2 = self.fishing_pot.deal_n_cards(7) #redundant
+        amt_of_cards = 7 #this may vary with version of GoFish rules
+        amt_dealt_init_hand1 = self.fishing_pot.deal_n_cards(amt_of_cards)
+        amt_dealt_init_hand2 = self.fishing_pot.deal_n_cards(amt_of_cards) #redundant
 
         self.player1.stack.accept_n_cards(amt_dealt_init_hand1)
         self.player2.stack.accept_n_cards(amt_dealt_init_hand2)
+        print "dealing {} cards to each player".format(amt_of_cards)
 
     def computer_guesser(self):
         #guess_card = random.random(len(self.fishing_pot))
@@ -76,21 +79,27 @@ class GoFishGame(object):
         else:
             "you both lose"
 
+# TM 040615
     def run_round(self):
         if self.fishing_pot: #if there aren't any cards in the fishing pot compare books and game ends
             card_rank_guess = self.computer_guesser()
-            not_cur_player = self.cur_player_deq[-1]
+            not_cur_player = self.cur_player_deq[-1] #starting to think about mult players
             if card_rank_guess in list(not_cur_player.stack.ranks):
                 pass
             self.cur_player_deq.rotate()
 
         else:
             self.compare_books()
-
+#end TM 040615
     def check_hand_for_books(self): #put logic in org_cards
         cur_player = self.cur_players[0]
         for card in cur_player.stack.deck:
             pass
+        c = Counter(cur_player.stack.deck)
+        lis = list()
+        for i in c.itervalues():
+            if c[i] == 4:
+                lis.append(c.pop[i])
 
     def go_fish(self):
         cur_player = self.cur_players[0]
