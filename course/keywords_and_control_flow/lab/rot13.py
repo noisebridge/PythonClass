59577a92753e4aedf5d6de643330bf97ed18e1c2
Trent Robbins <robbintt@gmail.com>
2015-05-28 20:29:52 -0700

    Finished basic rot13, still have edge cases to fix.

--- course/keywords_and_control_flow/lab/rot13.py
+++ course/keywords_and_control_flow/lab/rot13.py
@@ -12,12 +12,32 @@ wikipedia_result = "Jul qvq gur puvpxra pebff gur ebnq?"
 
 
 def encode_with_rot13(message):
-    """ Accepts a string. encodes it and returns the result.
+    """ Accepts a string. encodes it and returns the result string.
     
     (More detailed explanation to come)
-    """
-    
+    Steps:
+    1. Accept the string as message.
+    2a. Loop through the characters in the string.
+    2b. If it is a letter 'apply the rot13 cipher' to the letter.
+    2c. Else store the character without any change.
+    3. Return the string.
     
 
+    """
+    # We are ignoring case, we will use lowercase for all.
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    result_list = list()
+    for character in message:
+        if character.lower() in alphabet:
+            rotated_index = (alphabet.index(character.lower()) + 13) % 26
+            result_list.append(alphabet[rotated_index])
+        else:
+            result_list.append(character)
+    result = ''.join(result_list)
+    return result
+
+
+print(encode_with_rot13(message))
 
 
