2071825139ea12176c915eb8a688784233671363
Trent Robbins <robbintt@gmail.com>
2015-05-28 20:38:11 -0700

    Completed support for preserving capitalization.

--- course/keywords_and_control_flow/lab/rot13.py
+++ course/keywords_and_control_flow/lab/rot13.py
@@ -7,8 +7,8 @@ Goal: Encode a message using rot13.
 # Where is the message going to be coming from?
 # Test case from wikipedia:
 # http://en.wikipedia.org/wiki/ROT13
-message = "Why did the chicken cross the road?"
-wikipedia_result = "Jul qvq gur puvpxra pebff gur ebnq?"
+#message = "Why did the chicken cross the road?"
+#wikipedia_result = "Jul qvq gur puvpxra pebff gur ebnq?"
 
 
 def encode_with_rot13(message):
@@ -26,17 +26,23 @@ def encode_with_rot13(message):
     """
     # We are ignoring case, we will use lowercase for all.
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    
+    # Convenience reference.
+    alphabet_upper = alphabet.upper()
+
     result_list = list()
     for character in message:
-        if character.lower() in alphabet:
-            rotated_index = (alphabet.index(character.lower()) + 13) % 26
+        if character in alphabet:
+            rotated_index = (alphabet.index(character) + 13) % 26
             result_list.append(alphabet[rotated_index])
+        elif character in alphabet_upper:
+            rotated_index = (alphabet_upper.index(character) + 13) % 26
+            result_list.append(alphabet_upper[rotated_index])
         else:
             result_list.append(character)
     result = ''.join(result_list)
     return result
 
+message = raw_input("Enter your message:")
 
 print(encode_with_rot13(message))
 
