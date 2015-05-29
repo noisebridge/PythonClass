45df0db0b89ffc5a6b9a1aa4a71f09ae053b9c1f
Trent Robbins <robbintt@gmail.com>
2015-05-28 19:30:49 -0700

    Began rot13 cipher.

---
+++ course/keywords_and_control_flow/lab/rot13.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8
+"""
+Goal: Encode a message using rot13.
+
+"""
+
+# Where is the message going to be coming from?
+# Test case from wikipedia:
+# http://en.wikipedia.org/wiki/ROT13
+message = "Why did the chicken cross the road?"
+wikipedia_result = "Jul qvq gur puvpxra pebff gur ebnq?"
+
+
+def encode_with_rot13(message):
+    """ Accepts a string. encodes it and returns the result.
+    
+    (More detailed explanation to come)
+    """
+    
+    
+
+
+
