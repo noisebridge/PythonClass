bd44f6d74e6c03eabbcf22754a928cf04817929b
Trent Robbins <robbintt@gmail.com>
2015-07-23 20:49:12 -0700

    Added some code to open a sample file.

--- course/regular_expressions/project/email_checker.py
+++ course/regular_expressions/project/email_checker.py
@@ -15,10 +15,36 @@ rough_email_validator = r'([^@|\s]+@[^@]+\.[^@|\s]+)'
 
 email_validator_re = re.compile(rough_email_validator)
 
+def build_emails_from_sample_file(filename):
+    """
+    """
+
+    with open(filename, 'r') as f:
+        l = list()
+        for line in f:
+            l.append(line.rstrip())
+
+    if len(l) < 1:
+        raise Exception, "The file had no data."
+
+    return l
 
 
 if __name__ == '__main__':
+    """ Run tests for our module.
+    """
 
+    SAMPLE_SOURCE_FILE = "source.txt"
+
+    for email in build_emails_from_sample_file(SAMPLE_SOURCE_FILE):
+        
+        if email_validator_re.match(email):
+            print "Valid email: {}".format(email)
+        else:
+            print "Not a valid email: {}".format(email)
+
+
+    """
     initial_email = "email@email.email"
     bad_email = "me@@@test"
 
@@ -31,5 +57,5 @@ if __name__ == '__main__':
         print "Valid email: {}".format(bad_email)
     else:
         print "Not a valid email: {}".format(bad_email)
-
+    """
 
