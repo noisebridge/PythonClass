bfbac30da0a0f8409f70d366823143536fb52aff
Trent Robbins <robbintt@gmail.com>
2015-07-23 21:02:55 -0700

    Finished basic goals for today.

--- course/regular_expressions/project/email_checker.py
+++ course/regular_expressions/project/email_checker.py
@@ -30,32 +30,37 @@ def build_emails_from_sample_file(filename):
     return l
 
 
+def write_list_to_file(mylist, output_filename):
+    """
+    """
+
+    import json # move this to the top
+
+    with open(output_filename, 'w') as f:
+        json.dump(mylist, f)
+
+
 if __name__ == '__main__':
     """ Run tests for our module.
     """
 
     SAMPLE_SOURCE_FILE = "source.txt"
 
+    VALID_OUTPUT_FILE = "valid.txt"
+    INVALID_OUTPUT_FILE = "invalid.txt"
+    
+    VALID_EMAILS = list()
+    INVALID_EMAILS = list()
+
     for email in build_emails_from_sample_file(SAMPLE_SOURCE_FILE):
         
         if email_validator_re.match(email):
             print "Valid email: {}".format(email)
+            VALID_EMAILS.append(email)
         else:
             print "Not a valid email: {}".format(email)
-
-
-    """
-    initial_email = "email@email.email"
-    bad_email = "me@@@test"
-
-    if email_validator_re.match(initial_email):
-        print "Valid email: {}".format(initial_email)
-    else:
-        print "Not a valid email: {}".format(initial_email)
-
-    if email_validator_re.match(bad_email):
-        print "Valid email: {}".format(bad_email)
-    else:
-        print "Not a valid email: {}".format(bad_email)
-    """
+            INVALID_EMAILS.append(email)
+    
+    write_list_to_file(VALID_EMAILS, VALID_OUTPUT_FILE)
+    write_list_to_file(INVALID_EMAILS, INVALID_OUTPUT_FILE)
 
