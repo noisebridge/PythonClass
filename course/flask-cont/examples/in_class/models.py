1c049da4c68177b0cabe996604e409a28fbd2eb0
teddymcw <teddymcw@gmail.com>
2015-08-10 21:00:55 -0700

    added Posts models

--- course/flask-cont/examples/in_class/models.py
+++ course/flask-cont/examples/in_class/models.py
@@ -19,6 +19,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(120), unique=False)
     email = db.Column(db.String(120), unique=True)
     date_created = datetime.now()
+    post_owner = db.relationship('Post', backref='hack_news_user')
 
     def __init__(self, username, password, email):
         self.username = username
@@ -27,4 +28,24 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {0}, Email: {1}>'.format(self.username, self.email)
+
+
+
+class Post(db.Model):
+
+    __tablename__ = "hack_news_posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    domain = db.Column(db.String(120))
+    points = db.Column(db.Integer, nullable=True)
+    username = db.Column( db.ForeignKey("hack_news_users.username") )
+
+    def __init__(self, title, domain, username):
+        self.title = title
+        self.domain = domain 
+        self.username = username
+
+    def __repr__(self):
+        return "title: {0}, poster username: {1}".format(self.title, self.username)
 
