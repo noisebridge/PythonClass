f592bad093da4c1ad94aaad98e2e91fde87b6777
Trent Robbins <robbintt@gmail.com>
2015-09-28 20:48:06 -0700

    Finished while loop to check window lengths.

--- projects/compress/substringwindow.py
+++ projects/compress/substringwindow.py
@@ -3,7 +3,7 @@ Illustrate a comparison window for a substring
 """
 
 
-mystring = 'cacaca'
+mystring = 'cacacacacaca'
 
 """
 Initial window size is 1/2 the total length of the string
@@ -25,32 +25,40 @@ we will calculate the next windowsize by subtracting 1 from
 the current window size.
 
 """
-window_size_2 = 2 # hard coded the next windowsize
+
+# find the new window size from the initial window size
+window_size = init_window_size - 1
 
 # we will check substr1 against all the other substrings.
-substr1 = mystring[0:window_size_2]
+substr1 = mystring[0:window_size]
 
-substr2 = mystring[window_size_2:(window_size_2+window_size_2)]
+substr2 = mystring[window_size:(window_size+window_size)]
 
 # increment by 1 for the next substring to check
 # (we can keep incrementing the window position by 1 
 # until we hit the top)
-substr3 = mystring[window_size_2+1:(window_size_2+window_size_2+1)]
+substr3 = mystring[window_size+1:(window_size+window_size+1)]
 
-substr4 = mystring[window_size_2+1+1:(window_size_2+window_size_2+1+1)]
+substr4 = mystring[window_size+1+1:(window_size+window_size+1+1)]
 
-print substr1
-print substr2
+# print substr1
+# print substr2
+# 
+# print substr1 == substr2
+# print substr1 == substr3
+# print substr1 == substr4
+# 
 
-print substr1 == substr2
-print substr1 == substr3
-print substr1 == substr4
-
+# window lower bound starts right after the window we are checking against
+window_lower_bound = window_size
+target_substring = mystring[0:window_size]
 
 # while loop (while the second value is less than the total)
+while (window_lower_bound + window_size) <= len(mystring):
 
+    current_substring_to_check = mystring[window_lower_bound:(window_size+window_lower_bound)] 
+    window_lower_bound += 1
 
-
-
+    print target_substring == current_substring_to_check
 
 
