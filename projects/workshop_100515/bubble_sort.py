836c9dd35d4591f3e400ec70b30d206046c133f2
Trent Robbins <robbintt@gmail.com>
2015-10-05 20:09:19 -0700

    Finished bubble sort.

--- projects/workshop_100515/bubble_sort.py
+++ projects/workshop_100515/bubble_sort.py
@@ -1,6 +1,8 @@
 """ Bubble Sort 
 
 """
+import random
+import time
 
 def compare_two_numbers(a, b):
     """ Return the two numbers as a tuple, smallest to largest.
@@ -14,12 +16,18 @@ def compare_two_numbers(a, b):
 
     return result
 
+mylist = range(1,21)
+random.shuffle(mylist)
+print mylist
 
-mylist = [4,2,3,1]
+#mylist = [4,2,3,1]
 
-# lets iterate over the list length minus one times.
-for i in range(len(mylist)-1):
-    mylist[i], mylist[i+1] = compare_two_numbers(mylist[i], mylist[i+1])
+for j in range(len(mylist)-1):
+    # lets iterate over the list length minus one times.
+    for i in range(len(mylist)-j-1):
+        print mylist
+        mylist[i], mylist[i+1] = compare_two_numbers(mylist[i], mylist[i+1])
+        time.sleep(.4)
 
 print mylist
 
