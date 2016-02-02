e5199eb093208eb23b42cf1533720c73af3f3fc1
Trent Robbins <robbintt@gmail.com>
2016-02-01 17:59:45 -0800

    Added updated wiki readme conversion. Updated writesample in sqlite3.

--- course/sqlite3_module/examples/writesample.py
+++ course/sqlite3_module/examples/writesample.py
@@ -3,9 +3,9 @@ Write a sample directly from a Python list of lists.
 """
 import sqlite3
 
-dbdir = "db/"
+dbdir = "data/"
 
-sql_filename = 'sampledb.sqlite'
+sql_filename = 'craigslistdata.sqlite'
 
 sqlite_fileandpath = dbdir + sql_filename
 
@@ -51,3 +51,38 @@ if __name__ == '__main__':
         conn.close()
 
 
+"""
+
+"""
+import sqlite3
+
+dbdir = "db/"
+
+sql_filename = 'sampledb.sqlite'
+
+sqlite_fileandpath = dbdir + sql_filename
+
+if __name__ == '__main__':
+
+    # DB connection per file.
+    conn = sqlite3.connect(sqlite_fileandpath)
+
+    # return bytestrings instead of unicode
+    # https://docs.python.org/2/library/sqlite3.html#sqlite3.Connection.text_factory
+    conn.text_factory = str
+
+    c = conn.cursor()
+    
+    c.execute("SELECT * from OfficeSupplies;")
+
+    result_list = c.fetchall()
+
+    conn.close()
+
+    # This is returned as a list of tuples, howeever SQL specifies a row as an 
+    # unordered group of values defined by their attribute name (column heading).  
+    # In this way, a SQL row is more like a set of key-value pairs. The keys are 
+    # shared by all other rows in the group/table.
+
+    for result in result_list:
+        print result
