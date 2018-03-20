33b682216a6d02d4fd310c7f7b25bb1e8c72be36
Razzi Abuissa <razzi53@gmail.com>
2018-03-19 18:19:53 -0700

    Heroku config

--- guest-talks/2018-django/retrosocial/retrosocial/settings.py
+++ guest-talks/2018-django/retrosocial/retrosocial/settings.py
@@ -88,7 +88,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
